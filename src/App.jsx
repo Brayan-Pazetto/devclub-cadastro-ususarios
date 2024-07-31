c8e6f4c1b9afd988a4240bd9e6aaf016e660071c
Brayan Pazetto <brayan.pazetto@gmail.com>
2024-07-31 17:29:04 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { Button, Container, ContainerInputs, Form, Input, InputLabel, Title, TopBackground } from './styles.js'
+
+function Home() {
+  const myText = 'ASDASDUNAIUDNIAU'
+
+  return (
+
+    <Container>
+      <TopBackground>
+        <img src="" alt="" />
+      </TopBackground>
+
+      <Form>
+        <Title> Cadastrar Usuários </Title>
+
+        <ContainerInputs>
+          <div>
+            <div>
+              <InputLabel>
+                Nome<span>*</span>
+              </InputLabel>
+              <Input type='text' placeholder='Nome do usuario' />
+            </div>
+            <div>
+              <InputLabel>
+                Idade<span>*</span>
+              </InputLabel>
+              <Input type='text' placeholder='Idade do usuario' />
+            </div>
+            <div>
+              <InputLabel>
+                Email<span>*</span>
+              </InputLabel>
+              <Input type='text' placeholder='Email do usuario' />
+            </div>
+
+          </div>
+        </ContainerInputs>
+
+        <Button>Cadastrar usuario</Button>
+
+
+      </Form>
+
+    </Container>
+
+  )
+}
+
+export default Home
